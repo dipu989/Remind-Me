d574707d80786b282885c855d020cbef387ab050
Shantnu Kumar <me.shantnu@gmail.com>
2019-01-19 23:15:47 +0530

    Improve main screen.

--- app/src/main/java/com/example/shantnukumar/remind_me/HomeActivity.java
+++ app/src/main/java/com/example/shantnukumar/remind_me/HomeActivity.java
@@ -2,12 +2,29 @@ package com.example.shantnukumar.remind_me;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        listView = findViewById(R.id.listView);
+        ArrayList<String> tasks = new ArrayList<>();
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,tasks);
+        listView.setAdapter(arrayAdapter);
+        tasks.add("Get this app ready!");
+
+        if (arrayAdapter.getCount() == 0) {
+
+            tasks.add("Add Tasks");
+        }
     }
 }
