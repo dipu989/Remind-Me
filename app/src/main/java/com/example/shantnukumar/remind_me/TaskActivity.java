34738e147efd3f2c232baf5eaa08029195e1b970
Shantnu Kumar <me.shantnu@gmail.com>
2019-01-21 18:41:58 +0530

    New Task UI and Main Screen UI improvement.

---
+++ app/src/main/java/com/example/shantnukumar/remind_me/TaskActivity.java
@@ -0,0 +1,58 @@
+package com.example.shantnukumar.remind_me;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class TaskActivity extends AppCompatActivity {
+
+    Button selectDate;
+    TextView date;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+        selectDate = (Button) findViewById(R.id.datePicker);
+        date = (TextView) findViewById(R.id.dateTextView);
+
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(TaskActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        date.setText(dayOfMonth + "/" + (month +1) + "/" + year);
+                    }
+                },year,month,dayOfMonth);
+                datePickerDialog.show();
+            }
+        });
+        }
+
+
+        public void finishActivity(View view) {
+            finish();
+        }
+
+
+}
+
